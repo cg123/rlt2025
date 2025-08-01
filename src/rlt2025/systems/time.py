4f5b559d410e837e311212db4fb6c52b5ba758e2
Charles Goddard <chargoddard@gmail.com>
2025-08-02 01:16:45 +0200

    Fix

--- src/rlt2025/systems/time.py
+++ src/rlt2025/systems/time.py
@@ -1,15 +1,15 @@
 from rlt2025.components import ScheduledEvent
-from rlt2025.engine import Engine
+from rlt2025.ecs import World
 from rlt2025.events import TimeAdvanceRequestEvent, TimeElapsedEvent
 
 
 class TimeSystem:
-    def __init__(self, engine: Engine):
-        engine.event_bus.register(TimeAdvanceRequestEvent, self.advance_time)
+    def __init__(self, world: World):
+        world.event_bus.register(TimeAdvanceRequestEvent, self.advance_time)
 
-    def advance_time(self, event: TimeAdvanceRequestEvent, engine: Engine) -> None:
-        current_tick = engine.tick_count
-        waiting = list(engine.entities.query(ScheduledEvent))
+    def advance_time(self, event: TimeAdvanceRequestEvent, world: World) -> None:
+        current_tick = world.tick_count
+        waiting = list(world.entities.query(ScheduledEvent))
         if not waiting:
             return
 
@@ -17,15 +17,16 @@ class TimeSystem:
         min_ent = None
         for entity, component in waiting:
             next_turn = component.tick
-            if min_ent is None or next_turn < min_delay:
+            if min_ent is None or min_delay is None or next_turn < min_delay:
                 min_delay = next_turn
                 min_ent = entity
 
         if min_ent is not None:
-            c = engine.entities.get_component(min_ent, ScheduledEvent)
-            engine.event_bus.post(c.event)
+            c = world.entities.get_component(min_ent, ScheduledEvent)
+            assert c is not None, "ScheduledEvent component is None"
+            world.event_bus.post(c.event)
             if c.tick > current_tick:
-                engine.tick_count = c.tick
+                world.tick_count = c.tick
                 elapsed = c.tick - current_tick
-                engine.event_bus.post(TimeElapsedEvent(elapsed_ticks=elapsed))
-            engine.entities.remove_component(min_ent, ScheduledEvent)
+                world.event_bus.post(TimeElapsedEvent(elapsed_ticks=elapsed))
+            world.entities.remove_component(min_ent, ScheduledEvent)
